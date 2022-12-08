0e216a4414287c240611558daf8f07d10848c286
Roger Yen <rogeryen@gmail.com>
2022-12-07 22:07:16 -0500

    easy day 6

---
+++ day6/part2.rb
@@ -0,0 +1,13 @@
+text = File.read("./input.txt")
+i = 0
+j = 13
+
+while j < text.length
+  new_array = text[i..j].chars
+  if new_array.uniq == new_array
+    p j + 1
+    return
+  end
+  i += 1
+  j += 1
+end
